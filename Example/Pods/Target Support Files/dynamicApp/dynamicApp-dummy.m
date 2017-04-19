4127fd87316fa9d04ce3b8663acd88cb6b685be3
Satish Mavani <satish.mavani@motifworks.com>
2017-04-19 20:51:52 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/dynamicApp/dynamicApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_dynamicApp : NSObject
+@end
+@implementation PodsDummy_dynamicApp
+@end
---
+++ Example/Pods/Target Support Files/dynamicApp/dynamicApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double dynamicAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char dynamicAppVersionString[];
+
